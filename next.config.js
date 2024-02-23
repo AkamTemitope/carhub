026d658a01a7e96280f8411ce5c9f032adc48fdc
Akamtemitope <temitopeakam@gmail.com>
2024-02-23 04:05:58 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["cdn.imagin.studio", "images.pexels.com"]
-    }
-}
+  images: {
+    domains: [
+      "pixabay.com",
+      "cdn.pixabay.com",
+      "images.pexels.com",
+      "cdn.imagin.studio"
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
